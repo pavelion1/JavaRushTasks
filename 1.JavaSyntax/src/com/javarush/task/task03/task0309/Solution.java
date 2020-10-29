40967c2784bb4e982e548af395684590defc89a6
Pasha <shpuntp@gmail.com>
2020-10-29 12:44:46 +0300

    Я решил 5 задач

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0309/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task03.task0309;
+
+/* 
+Сумма 5 чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println(1);
+        System.out.println(3);
+        System.out.println(6);
+        System.out.println(10);
+        System.out.println(15);
+
+    }
+}
