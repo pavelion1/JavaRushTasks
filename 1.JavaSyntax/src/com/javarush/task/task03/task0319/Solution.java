b958b947df51a95bc35ba74857c67815cadae282
Pasha <shpuntp@gmail.com>
2020-10-29 22:34:59 +0300

    добавил служебные файлы

--- 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
@@ -9,6 +9,13 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader  (System.in));
+        String name = bufferedReader.readLine();
+        int int1 = Integer.parseInt(bufferedReader.readLine());
+        int int2 = Integer.parseInt(bufferedReader.readLine());
+        System.out.println(name + " получает " +int1+ " через " +int2+ " лет.");
+
+
 
 
     }
